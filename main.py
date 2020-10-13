b669c1916d4b9c7922b8afa032c7345c21ed4537
username <54479676+CormacKrum@users.noreply.github.com>
2020-10-14 02:14:04 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import json
+from difflib import get_close_matches
 
 dictionary = dict()
+
+with open('data.json') as f:
+    dictionary = json.load(f)
 
 
 def printResult(modified_word):
@@ -8,17 +12,18 @@ def printResult(modified_word):
         print(str(i + 1) + "." + dictionary[modified_word][i])
 
 
-with open('data.json') as f:
-    dictionary = json.load(f)
-
-word = input("Enter A Word:\n")
-if word in dictionary:
-    printResult(word)
-elif word.title() in dictionary:
-    printResult(word.title())
-elif word.upper() in dictionary:
-    printResult(word.upper())
-elif word.lower() in dictionary:
-    printResult(word.lower())
-else:
-    print("Oops Result Not Found")
+input_word = input("Enter A Word:\n")
+words = get_close_matches(input_word, [key for key, value in dictionary.items()])
+for word in words:
+    if word != input_word:
+        print("Did You Mean \033[1m" + word + "\033[0m")
+    if word in dictionary:
+        printResult(word)
+    elif word.title() in dictionary:
+        printResult(word.title())
+    elif word.upper() in dictionary:
+        printResult(word.upper())
+    elif word.lower() in dictionary:
+        printResult(word.lower())
+    else:
+        print("Oops Result Not Found")
