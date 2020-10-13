e3ae0eeec288838a3ed83d9e26dbb07b0040179f
username <54479676+CormacKrum@users.noreply.github.com>
2020-10-14 01:58:05 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import json
 
 dictionary = dict()
+
+
+def printResult(modified_word):
+    for i in range(len(dictionary[modified_word])):
+        print(str(i + 1) + "." + dictionary[modified_word][i])
+
+
 with open('data.json') as f:
     dictionary = json.load(f)
 
 word = input("Enter A Word:\n")
 if word in dictionary:
-    for i in range(len(dictionary[word])):
-        print(str(i)+"."+ dictionary[word][i])
+    printResult(word)
+elif word.title() in dictionary:
+    printResult(word.title())
+elif word.upper() in dictionary:
+    printResult(word.upper())
+elif word.lower() in dictionary:
+    printResult(word.lower())
 else:
     print("Oops Result Not Found")
