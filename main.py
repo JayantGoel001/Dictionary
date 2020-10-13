5be3f3db2a68c06b78e098835b14fd0baf1608b2
username <54479676+CormacKrum@users.noreply.github.com>
2020-10-14 02:23:08 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -10,20 +10,26 @@ with open('data.json') as f:
 def printResult(modified_word):
     for i in range(len(dictionary[modified_word])):
         print(str(i + 1) + "." + dictionary[modified_word][i])
+    print()
 
 
 input_word = input("Enter A Word:\n")
 words = get_close_matches(input_word, [key for key, value in dictionary.items()])
-for word in words:
-    if word != input_word:
-        print("Did You Mean \033[1m" + word + "\033[0m")
-    if word in dictionary:
-        printResult(word)
-    elif word.title() in dictionary:
-        printResult(word.title())
-    elif word.upper() in dictionary:
-        printResult(word.upper())
-    elif word.lower() in dictionary:
-        printResult(word.lower())
-    else:
-        print("Oops Result Not Found")
+if len(words) > 0:
+    for word in words:
+        if word != input_word:
+            print("Did You Mean \033[1m" + word + "\033[0m?")
+        if word in dictionary:
+            printResult(word)
+        elif word.title() in dictionary:
+            printResult(word.title())
+        elif word.upper() in dictionary:
+            printResult(word.upper())
+        elif word.lower() in dictionary:
+            printResult(word.lower())
+        else:
+            print("Oops Result Not Found")
+        if word==input_word:
+            break
+else:
+    print("Oops Result Not Found")
